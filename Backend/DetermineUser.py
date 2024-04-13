3a354ef2fd8ee032a34c26456415ea82a77b27f9
Swabhan Katkoori <swabhankatkoor@icloud.com>
2024-04-13 11:02:32 -0400

    Intial Setup for User Preference Determination

---
+++ Backend/DetermineUser.py
@@ -0,0 +1,30 @@
+
+class DetermineUser:
+    def __init__(self):
+        toJSON = {}
+
+    def swipe(self, yes):
+        '''
+        Used to determine if user likes activity
+        '''
+        pass
+
+    def recieveActivity(self):
+        '''
+        Generate activity from Yelp API
+        '''
+        pass
+
+    def categorizeActivity(self):
+        '''
+        Categorizes activity
+        '''
+        pass
+
+    def addToJSON(self):
+        '''
+        Following activity categorization and swipe, add to JSON with user preference
+        '''
+        pass
+
+    

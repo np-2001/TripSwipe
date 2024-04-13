872eb5ffbe9d2f83308b027db4f0953cfc3de76c
Swabhan Katkoori <swabhankatkoor@icloud.com>
2024-04-13 11:40:16 -0400

    Allow Activity Reception from Yelp API

--- Backend/DetermineUser.py
+++ Backend/DetermineUser.py
@@ -1,3 +1,23 @@
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+from urllib.parse import quote
+import pprint
+
+
+YELP_API_KEY= "RytzLV4FkRl2FcHDc9nGBYu8ZbQBXneST-ddi-U6EUUwpe_9bEEyx_IzUzZPShmOcUGrOwPK5YEW9ZVIMxnl71jEsVFqApqRzn7uXV5l5xau_2_jY_c88_85q6MaZnYx"
+
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'
+
+DEFAULT_TERM = 'dinner'
+DEFAULT_LOCATION = 'San Francisco, CA'
+SEARCH_LIMIT = 3
+
 
 class DetermineUser:
     def __init__(self):
@@ -13,7 +33,22 @@ class DetermineUser:
         '''
         Generate activity from Yelp API
         '''
-        pass
+        url_params = {
+            'term': DEFAULT_TERM .replace(' ', '+'),
+            'location': DEFAULT_LOCATION.replace(' ', '+'),
+            'limit': SEARCH_LIMIT
+        }
+        url = '{0}{1}'.format(API_HOST, quote(SEARCH_PATH.encode('utf8')))
+
+        headers = {
+            'Authorization': 'Bearer %s' % YELP_API_KEY,
+        }
+
+        print(u'Querying {0} ...'.format(url))
+
+        response = requests.request('GET', url, headers=headers, params=url_params)
+
+        return response.json()
 
     def categorizeActivity(self):
         '''
@@ -28,3 +63,9 @@ class DetermineUser:
         pass
 
     
+def main():
+    user = DetermineUser()
+    pprint.pprint(user.recieveActivity())
+
+if __name__ == '__main__':
+    main()
